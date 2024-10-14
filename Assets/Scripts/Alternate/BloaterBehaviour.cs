a0113720532914a143fd2d5ec8c86c7d5752a043
PerplexedVoid <douglas.vignelles@gmail.com>
2024-10-14 19:20:13 +1100

    Enemy AI!!
    Bloater animation and pathfinding.

---
+++ Assets/Scripts/Alternate/BloaterBehaviour.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BloaterBehaviour : MonoBehaviour
+{
+    // For navigation and pathfinding 
+    private NavMeshAgent agent;
+    private GameObject player;
+    private Vector3 targetVector;
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    private bool followPlayer = false;
+
+    // For animation
+    private Animator animator;
+    public float speed;
+    private Vector3 lastPosition;
+    private float animationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        lastPosition = transform.position;
+        targetVector = endPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calcualte speed for animation purposes
+        float actualSpeed = Vector3.Distance(transform.position, lastPosition) / Time.deltaTime;
+        animationSpeed = actualSpeed / speed;
+        animator.SetFloat("Speed", animationSpeed);
+        lastPosition = transform.position;
+
+        // FOV code
+        InFOV();
+
+        // Navigation code
+        if (followPlayer) {
+            agent.destination = player.transform.position;
+        } else {
+            agent.destination = targetVector;
+            float distance = Vector3.Distance(transform.position, targetVector);
+            if (distance < 1f) {
+                targetVector = (targetVector == startPoint) ? endPoint : startPoint;
+            }
+        }
+    }
+
+    void InFOV() {
+        // Check player is in enemy FOV
+        // Vector3 bloaterForward = transform.forward;
+        // Vector3 playerForward = player.transform.forward;
+        // Vector3 directionBetween = (player.transform.position - transform.position).normalized;
+        // float dotProduct1 = Vector3.Dot(bloaterForward, directionBetween);
+        // float dotProduct2 = Vector3.Dot(playerForward, -directionBetween);
+        // float threshold = 15f; // Adjust for desired accuracy
+        // bool arePointingAtEachOther = (dotProduct1 > threshold && dotProduct2 > threshold);
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance < 12f) {
+            followPlayer = true;
+        } else {
+            followPlayer = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Alternate/SpawnManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject bloaterPrefab; // bloaters are fat and slow. Conversely, they deal and can take significant damage
+    public GameObject stalkerPrefab; // stalkers are quiet and more persistent. they have larger 'patrol' areas and search for the player more thoroughly given their last location 
+    public GameObject walkerPrefab; // walkers are the quintessential infected. slow, low damage. however, they tend to swarm
+    public GameObject shriekerPrefab; // shreikers are large, fast, and loud. only known method of deterrence is to run, hide and pray. 
+
+    private BloaterBehaviour bloaterScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SpawnEnemy("bloater", new Vector3 (39,0,36), new Vector3 (14,0,12));
+        SpawnEnemy("bloater", new Vector3 (39,0,36), new Vector3 (39,0,28));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    // Spawns 
+    void SpawnEnemy(string enemyType, Vector3 startPoint, Vector3 endPoint) {
+        if (enemyType == "bloater") {
+            GameObject bloaterInstance = Instantiate(bloaterPrefab, startPoint, bloaterPrefab.transform.rotation);
+            bloaterScript = bloaterInstance.GetComponent<BloaterBehaviour>();
+            bloaterScript.startPoint = startPoint;
+            bloaterScript.endPoint = endPoint;
+        }
+    }
+}
