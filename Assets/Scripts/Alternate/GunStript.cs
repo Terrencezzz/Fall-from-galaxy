3ae21701ad8ae6855687094331500e7136589cb4
Wanzhong Wu <u7642453@anu.edu.au>
2024-10-15 23:14:45 +1100

    Improve gun script

--- Assets/Scripts/Alternate/GunStript.cs
+++ Assets/Scripts/Alternate/GunStript.cs
@@ -6,21 +6,58 @@ public class GunStript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
-    public float fireRate = 0.1f;
+    public float fireRate1 = 0.1f;
 
     public Camera fpsCam;
     public GameObject bulletHolePrefab;
+    public AudioSource shootingSound;
 
     private float nextTimeToShoot = 0f;
-    public AudioSource shootingSound;
+
+
+    public GameObject autoGun;
+    public GameObject currentGun;
+
+    bool autoGunActive = true;
+
+    private int gunType = 1;
+
+    bool aim = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToShoot)
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToShoot && gunType == 2)
         {
-            nextTimeToShoot = Time.time + fireRate;
+            nextTimeToShoot = Time.time + fireRate1;
             Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchGun(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchGun(2);
+        }
+    }
+
+    void SwitchGun(int type)
+    {
+        if (type != gunType)
+        {
+            if (type == 1 && currentGun != null)
+            {
+                Destroy(currentGun);
+            }
+            else if (type == 2 && autoGunActive)
+            {
+                if (!currentGun) Destroy(currentGun);
+                currentGun = Instantiate(autoGun, transform.position, transform.rotation);
+                currentGun.transform.SetParent(this.transform);
+            }
+            gunType = type;
         }
     }
 
@@ -30,8 +67,8 @@ public class GunStript : MonoBehaviour
         {
             shootingSound.Play();
         }
-        int layerMask = 1 << LayerMask.NameToLayer("Player");
-        layerMask = ~layerMask;
+
+        int layerMask = ~LayerMask.GetMask("Player");
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, layerMask))
