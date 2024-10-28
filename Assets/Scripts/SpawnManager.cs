e32aee3d6c6863f9bfa18e7966d07ba43996b2cf
PerplexedVoid <douglas.vignelles@gmail.com>
2024-10-28 13:43:16 +1100

    Bug fix tutorial

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -32,9 +32,32 @@ public class SpawnManager : MonoBehaviour
         SpawnEnemy("bloater", new Vector3(-45,0,60), new Vector3(-63,0,101));
         SpawnEnemy("bloater", new Vector3(-42,0,49), new Vector3(-48,0,35));
 
+        SpawnEnemy("bloater", new Vector3(63,0,77), new Vector3(49,0,77));
+        SpawnEnemy("bloater", new Vector3(58,0,63), new Vector3(40,0,60));
+        SpawnEnemy("bloater", new Vector3(47,0,65), new Vector3(47,0,89));
+
         SpawnEnemy("bloater", new Vector3(-39,12,18), new Vector3(-3,12,0));
-        SpawnEnemy("bloater", new Vector3(21,12,25), new Vector3(39,12,36));
-        SpawnEnemy("bloater", new Vector3(21,6,-36), new Vector3(39,6,36));
+        SpawnEnemy("bloater", new Vector3(21,12,25), new Vector3(39,12,-36));
+        SpawnEnemy("bloater", new Vector3(21,6,-36), new Vector3(39,6,-49));
+
+        SpawnEnemy("bloater", new Vector3(48,-12,17), new Vector3(39,-12,18));
+        SpawnEnemy("bloater", new Vector3(39,-12,20), new Vector3(40,-12,26));
+        SpawnEnemy("bloater", new Vector3(38,-12,27), new Vector3(40,-12,37));
+        SpawnEnemy("bloater", new Vector3(41,-12,47), new Vector3(47,-12,46));
+        SpawnEnemy("bloater", new Vector3(59,-12,46), new Vector3(67,-12,48));
+        SpawnEnemy("bloater", new Vector3(67,-12,41), new Vector3(69,-12,34));
+        SpawnEnemy("bloater", new Vector3(69,-12,32), new Vector3(57,-12,27));
+        SpawnEnemy("bloater", new Vector3(68,-12,17), new Vector3(59,-12,19));
+
+        SpawnEnemy("stalker", new Vector3(-57,0,42), new Vector3(-10,0,83));
+        SpawnEnemy("stalker", new Vector3(26,0,65), new Vector3(-21,0,19));
+        SpawnEnemy("stalker", new Vector3(-63,0,11), new Vector3(-32,0,19));
+        SpawnEnemy("stalker", new Vector3(74,0,-6), new Vector3(68,0,19));
+        SpawnEnemy("stalker", new Vector3(39,6,-54), new Vector3(-4,0,19));
+        SpawnEnemy("stalker", new Vector3(47,9,90), new Vector3(47,0,19));
+        SpawnEnemy("stalker", new Vector3(32,4.6f,59), new Vector3(15,0,19));
+
+
     }
 
     // Update is called once per frame
